251ee313b8353404899b3cf5ef86581a07c56919
Travis Martin <travisemerymartin@gmail.com>
2019-03-05 19:01:43 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -8,21 +8,22 @@ def bonus
          {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Steven", age: "30", attitude: "confused"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-          {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+          {name: "Mercutio", age: "18", attitude: "hot-headed"},
           {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
   }
 
   #code your solution here:
+  deaths_icy_grip="dead"
+  epic_tragedy.collect {|item| if item=="alive"; item=deaths_icy_grip end}
 
-  
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
